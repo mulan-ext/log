7b7bb3732010f1106a35b9e14226865af62bf20f
陌竹 <mozhu233@outlook.com>
2025-12-06 16:53:04 +0800

    🦄refactor(*): add Raw()

--- service.go
+++ service.go
@@ -9,10 +9,15 @@ type Service struct {
 	logger *Logger
 }
 
-func (s *Service) Serve() (err error) { s.logger, err = NewWithConfig(s.cfg); return }
+func (s *Service) Serve() (err error) {
+	s.logger, err = NewWithConfig(s.cfg)
+	return
+}
 
 func (s *Service) Shutdown(ctx context.Context) error { return s.logger.Close() }
 
 func (s *Service) Close() error { return s.logger.Close() }
+
+func (s *Service) Raw() any { return s.logger }
 
 func NewService(cfg *Config) *Service { return &Service{cfg: cfg} }
